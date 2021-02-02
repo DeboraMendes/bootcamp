86e4daebe06d72cf18546794562bc97833e778d6
DeboraMendes <debora.mendes@live.com>
2021-02-02 09:47:32 -0300

    exer3

--- src/br/com/bootcamp/exer3/Exer3.java
+++ src/br/com/bootcamp/exer3/Exer3.java
@@ -9,12 +9,28 @@ import java.time.Month;
 public class Exer3 {
 
     public static void main(String[] args) {
+        System.out.println("----------------Exer 2--------------");
+
+        System.out.println("---------Adicionar Pessoas----------");
         GrupoPessoa grupoPessoa = new GrupoPessoa();
-        Pessoa pessoa1 = new Pessoa("pessoa1", "abc", LocalDate.of(1998, Month.MARCH, 8));
-        Pessoa pessoa2 = new Pessoa("pessoa2", "abc", LocalDate.of(1998, Month.MARCH, 8));
-        Pessoa pessoa3 = new Pessoa("pessoa3", "abc", LocalDate.of(1998, Month.MARCH, 8));
-        grupoPessoa.adicionar(pessoa1);
-        grupoPessoa.adicionar(pessoa2);
-        grupoPessoa.adicionar(pessoa3);
+        grupoPessoa.adicionar(
+                new Pessoa("(Fez 18 anos ontem)", "01234567891", LocalDate.of(2003, Month.JANUARY, 28))
+        );
+        grupoPessoa.adicionar(
+                new Pessoa("(Está fazendo 18 anos hoje)", "01234567892", LocalDate.of(2003, Month.JANUARY, 29))
+        );
+        grupoPessoa.adicionar(
+                new Pessoa("(Vai fazer 18 anos amanhã)", "01234567893", LocalDate.of(2003, Month.JANUARY, 30))
+        );
+        grupoPessoa.adicionar(
+                new Pessoa("Debora", "01234567894", LocalDate.of(1998, Month.MARCH, 8))
+        );
+        System.out.println("---------Dados das Pessoas----------");
+        grupoPessoa.dadosDoGrupoPessoa();
+        System.out.println("--Dados das Pessoas com Maior Idade--");
+        grupoPessoa.dadosDoGrupoPessoaComMaiorIdade();
+        System.out.println("--Dados das Pessoas com Menor Idade--");
+        grupoPessoa.dadosDoGrupoPessoaComMenorIdade();
+        System.out.println("-------------------------------------");
     }
 }
--- src/br/com/bootcamp/exer3/grupoPessoa/GrupoPessoa.java
+++ src/br/com/bootcamp/exer3/grupoPessoa/GrupoPessoa.java
@@ -3,6 +3,7 @@ package br.com.bootcamp.exer3.grupoPessoa;
 import br.com.bootcamp.exer3.pessoa.Pessoa;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,6 +44,86 @@ public class GrupoPessoa {
 
     private LocalDate getDataAniversarioDe18Anos(LocalDate dataNascimento) {
         return dataNascimento.plusYears(18);
+    }
+
+    public void dadosDoGrupoPessoa() {
+        for (int i = 0; i < pessoaList.size(); i++) {
+            System.out.println(pessoaList.get(i));
+        }
+    }
+
+    public void dadosDoGrupoPessoaComMaiorIdade() {
+        Integer maiorIdade = getMaiorIdade();
+        if (maiorIdade != null) {
+            for (int i = 0; i < pessoaList.size(); i++) {
+                Integer idadeDaPessoaAtual = getIdade(pessoaList.get(i).getDataNascimento());
+                if (idadeDaPessoaAtual.equals(maiorIdade)) {
+                    System.out.println(pessoaList.get(i));
+                }
+            }
+        }
+
+    }
+
+    public void dadosDoGrupoPessoaComMenorIdade() {
+        Integer menorIdade = getMenorIdade();
+        if (menorIdade != null) {
+            for (int i = 0; i < pessoaList.size(); i++) {
+                Integer idadeDaPessoaAtual = getIdade(pessoaList.get(i).getDataNascimento());
+                if (idadeDaPessoaAtual.equals(menorIdade)) {
+                    System.out.println(pessoaList.get(i));
+                }
+            }
+        }
+    }
+
+    private Integer getMaiorIdade() {
+        if (existeAlgumaPessoaNaLista()) {
+            // inicializei a maior idade com a idade da primeira pessoa
+            int maiorIdade = getIdade(pessoaList.get(0).getDataNascimento());
+            // aqui eu posso começar com a segunda pessoa
+            for (int i = 1; i < pessoaList.size(); i++) {
+                Integer idadeDaPessoaAtual = getIdade(pessoaList.get(i).getDataNascimento());
+                //se a idade da pessoa atual for maior que a maior idade que eu tenho, vou atualizar a maior idade
+                if (idadeDaPessoaAtual > maiorIdade) {
+                    maiorIdade = idadeDaPessoaAtual;
+                }
+            }
+            return maiorIdade;
+        }
+        return null;
+    }
+
+    public Integer getMenorIdade() {
+        if (existeAlgumaPessoaNaLista()) {
+            // inicializei a menor idade com a idade da primeira pessoa
+            int menorIdade = getIdade(pessoaList.get(0).getDataNascimento());
+            // aqui eu posso começar com a segunda pessoa
+            for (int i = 1; i < pessoaList.size(); i++) {
+                Integer idadeDaPessoaAtual = getIdade(pessoaList.get(i).getDataNascimento());
+                //se a idade da pessoa atual for menor que a menor idade que eu tenho, vou atualizar a menor idade
+                if (idadeDaPessoaAtual < menorIdade) {
+                    menorIdade = idadeDaPessoaAtual;
+                }
+            }
+            return menorIdade;
+        }
+        return null;
+    }
+
+    /**
+     * null e empty (vazio) são coisas diferentes
+     * a pessoaList nunca será null porque no momento da sua declaração já instanciamos: pessoaList = new ArrayList<>();
+     * porém, se não for adicionada nenhuma Pessoa na pessoaList ela estará empty (vazia)
+     *
+     * @return
+     */
+    private boolean existeAlgumaPessoaNaLista() {
+        return !pessoaList.isEmpty();
+    }
+
+    private Integer getIdade(LocalDate dataNascimento) {
+        return Period.between(dataNascimento, dataAtual).getYears();
     }
 
 }
