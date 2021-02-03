e787ff0e90a3de92d8b59c785e0cd6072ba8970f
DeboraMendes <debora.mendes@live.com>
2021-02-03 13:20:45 -0300

    ajustes

--- src/br/com/bootcamp/aula/encapsulamento/exemp2/Calculadora.java
+++ src/br/com/bootcamp/aula/encapsulamento/exemp2/Calculadora.java
@@ -1,16 +1,23 @@
 package br.com.bootcamp.aula.encapsulamento.exemp2;
 
+//classe: Calculadora
 public class Calculadora {
 
+    //atributos privados do tipo inteiro
     private Integer numero1;
     private Integer numero2;
     private Integer resultado;
 
+    //método somar, recebe o numer1 e o número2
+    //realiza o cálculo e atribui o valor no resultado
+    //o atributo resultado não fica disponivel fora da classe
+    //somente essa classe pode alterar o resultado
     public void somar(Integer numero1,
                       Integer numero2) {
         resultado = numero1 + numero2;
     }
 
+    //mostra o resultado
     public Integer getResultado() {
         return resultado;
     }
--- src/br/com/bootcamp/aula/encapsulamento/exemp2/Principal.java
+++ src/br/com/bootcamp/aula/encapsulamento/exemp2/Principal.java
@@ -3,9 +3,18 @@ package br.com.bootcamp.aula.encapsulamento.exemp2;
 public class Principal {
 
     public static void main(String[] args) {
+
+        //criei uma calculadora
         Calculadora calculadora = new Calculadora();
+
+        //chamei o método somar
         calculadora.somar(2, 2);
+
+        //mostrei o resultado
+        //note que ao usar eu não consigo saber como o cálculo foi feito
+        //e também não tenho acesso para alterar o resultado
         System.out.println("Soma: " + calculadora.getResultado());
+
     }
 
 }
