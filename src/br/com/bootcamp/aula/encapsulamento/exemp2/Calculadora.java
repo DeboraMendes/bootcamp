11ab8452efa618def07a2863ba1c7e6782f54f58
DeboraMendes <debora.mendes@live.com>
2021-02-03 12:48:10 -0300

    ajustes

--- src/br/com/bootcamp/aula/encapsulamento/exemp2/Calculadora.java
+++ src/br/com/bootcamp/aula/encapsulamento/exemp2/Calculadora.java
@@ -4,24 +4,15 @@ public class Calculadora {
 
     private Integer numero1;
     private Integer numero2;
+    private Integer resultado;
 
-    public Integer somar(Integer numero1,
-                         Integer numero2) {
-        return numero1 + numero2;
+    public void somar(Integer numero1,
+                      Integer numero2) {
+        resultado = numero1 + numero2;
     }
 
-    public Integer subitrair(Integer numero1,
-                             Integer numero2) {
-        return numero1 - numero2;
+    public Integer getResultado() {
+        return resultado;
     }
 
-    public Integer multiplicar(Integer numero1,
-                               Integer numero2) {
-        return numero1 * numero2;
-    }
-
-    public double dividir(double numero1,
-                          double numero2) {
-        return numero1 / numero2;
-    }
 }
--- src/br/com/bootcamp/aula/encapsulamento/exemp2/Principal.java
+++ src/br/com/bootcamp/aula/encapsulamento/exemp2/Principal.java
@@ -4,10 +4,8 @@ public class Principal {
 
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        System.out.println(calculadora.somar(2, 2));
-        System.out.println(calculadora.subitrair(2, 2));
-        System.out.println(calculadora.multiplicar(2, 2));
-        System.out.println(calculadora.dividir(5, 2));
+        calculadora.somar(2, 2);
+        System.out.println("Soma: " + calculadora.getResultado());
     }
 
 }
